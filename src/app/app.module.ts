b659558a0dc4c37222e4954e5f152b1ff74b1573
Frank Rosner <frank@fam-rosner.de>
2020-05-24 11:52:52 +0200

    move components to components folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from '@src/app/app-routing.module';
-import { AppComponent } from '@src/app/app.component';
-import { HomeComponent } from '@src/app/home/home.component';
-import { SpotComponent } from '@src/app/spot/spot.component';
-import { GameComponent } from '@src/app/game/game.component';
+import {AppRoutingModule} from '@src/app/app-routing.module';
+import {AppComponent} from '@src/app/app.component';
+import {HomeComponent} from '@src/app/components/home/home.component';
+import {SpotComponent} from '@src/app/components/spot/spot.component';
+import {GameComponent} from '@src/app/components/game/game.component';
 
 
 @NgModule({
@@ -22,4 +22,5 @@ import { GameComponent } from '@src/app/game/game.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,17 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
-import { HomeComponent } from '@src/app/home/home.component';
-import {GameComponent} from '@src/app/game/game.component';
+import {HomeComponent} from '@src/app/components/home/home.component';
+import {GameComponent} from '@src/app/components/game/game.component';
 
 export const routes: Routes = [
   {
-      path: '',
-      redirectTo: '/home',
-      pathMatch: 'full',
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
   },
   {
-      path: 'home',
-      component: HomeComponent,
+    path: 'home',
+    component: HomeComponent,
   },
   {
     path: 'game',
