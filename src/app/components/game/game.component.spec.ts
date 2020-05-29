11f5f4bf5981130e31b164a5466686c366de0fbd
Frank Rosner <frank@fam-rosner.de>
2020-05-29 17:40:36 +0200

    dummy test

---
+++ src/app/components/game/game.component.spec.ts
@@ -0,0 +1,24 @@
+import {GameComponent} from '@src/app/components/game/game.component';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+
+describe('GameComponent', () => {
+  let component: GameComponent;
+  let fixture: ComponentFixture<GameComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [GameComponent],
+    })
+        .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(GameComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
