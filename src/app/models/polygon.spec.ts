642c2a0f8bf8b8a1b4965d278528b1ff88e0e3ee
Frank Rosner <frank@fam-rosner.de>
2020-05-31 13:58:35 +0200

    ray casting for square-in-polygon

--- src/app/models/polygon.spec.ts
+++ src/app/models/polygon.spec.ts
@@ -1,6 +1,7 @@
 import {Vertex} from '@src/app/models/vertex';
 import {Polygon} from '@src/app/models/polygon';
 import {Edge} from '@src/app/models/edge';
+import {Square} from '@src/app/models/square';
 
 describe('Polygon', () => {
   describe('fromPath', () => {
@@ -30,6 +31,139 @@ describe('Polygon', () => {
       ];
 
       expect(() => Polygon.fromPath(path)).toThrowError();
+    });
+  });
+
+  describe('contains', () => {
+    /*
+        A-----B
+        |     |
+        |  x~~+~~
+        |     |
+        C-----D
+    */
+    it('should return true if the square lies within a simple polygon', () => {
+      const A = new Vertex(0, 0, 0);
+      const B = new Vertex(1, 1, 0);
+      const C = new Vertex(2, 0, 1);
+      const D = new Vertex(3, 1, 1);
+      const polygon = Polygon.fromPath([A, B, D, C]);
+      const square = new Square(A, B, C, D, null);
+      expect(Polygon.contains(polygon, square)).toEqual(true);
+    });
+
+    /*
+        A-----B-----C-----D
+        |                 |
+        |                 |
+        |                 |
+        L     I-----H     E
+        |     |     |     |
+        |  x~~+~~~~~+~~~~~+~~
+        |     |     |     |
+        K-----J     G-----F
+    */
+    it('should return true if the square within a complex polygon such that the ray intersects more than once', () => {
+      const A = new Vertex(0, 0, 0);
+      const B = new Vertex(1, 1, 0);
+      const C = new Vertex(2, 2, 0);
+      const D = new Vertex(3, 3, 0);
+      const E = new Vertex(4, 3, 1);
+      const F = new Vertex(5, 3, 2);
+      const G = new Vertex(6, 2, 2);
+      const H = new Vertex(7, 2, 1);
+      const I = new Vertex(8, 1, 1);
+      const J = new Vertex(9, 1, 2);
+      const K = new Vertex(10, 0, 2);
+      const L = new Vertex(11, 0, 1);
+      const polygon = Polygon.fromPath([A, B, C, D, E, F, G, H, I, J, K, L]);
+      const square = new Square(L, I, K, J, null);
+      expect(Polygon.contains(polygon, square)).toEqual(true);
+
+    });
+
+    /*
+        A     B
+
+           x~~~~~~
+
+        C-----D
+        |     |
+        |     |
+        |     |
+        E-----F
+    */
+    it('should return false if the square lies above the polygon', () => {
+      const A = new Vertex(0, 0, 0);
+      const B = new Vertex(1, 1, 0);
+      const C = new Vertex(2, 0, 1);
+      const D = new Vertex(3, 1, 1);
+      const E = new Vertex(4, 0, 2);
+      const F = new Vertex(5, 1, 2);
+      const polygon = Polygon.fromPath([C, D, F, E]);
+      const square = new Square(A, B, C, D, null);
+      expect(Polygon.contains(polygon, square)).toEqual(false);
+    });
+
+    /*
+        A-----B
+        |     |
+        |     |
+        |     |
+        C-----D
+
+           x~~~~~~
+
+        E     F
+    */
+    it('should return false if the square lies below the polygon', () => {
+      const A = new Vertex(0, 0, 0);
+      const B = new Vertex(1, 1, 0);
+      const C = new Vertex(2, 0, 1);
+      const D = new Vertex(3, 1, 1);
+      const E = new Vertex(4, 0, 2);
+      const F = new Vertex(5, 1, 2);
+      const polygon = Polygon.fromPath([A, B, C, D]);
+      const square = new Square(C, D, E, F, null);
+      expect(Polygon.contains(polygon, square)).toEqual(false);
+    });
+
+    /*
+      A     B-----C
+            |     |
+         x~~+~~~~~+~~~
+            |     |
+      D     E-----F
+    */
+    it('should return false if the square lies left of the polygon', () => {
+      const A = new Vertex(0, 0, 0);
+      const B = new Vertex(1, 1, 0);
+      const C = new Vertex(2, 2, 0);
+      const D = new Vertex(3, 0, 1);
+      const E = new Vertex(4, 1, 1);
+      const F = new Vertex(5, 2, 1);
+      const polygon = Polygon.fromPath([B, C, F, E]);
+      const square = new Square(A, B, D, E, null);
+      expect(Polygon.contains(polygon, square)).toEqual(false);
+    });
+
+    /*
+        A-----B     C
+        |     |
+        |     |  x~~~~~~
+        |     |
+        D-----E     F
+    */
+    it('should return false if the square lies right of the polygon', () => {
+      const A = new Vertex(0, 0, 0);
+      const B = new Vertex(1, 1, 0);
+      const C = new Vertex(2, 2, 0);
+      const D = new Vertex(3, 0, 1);
+      const E = new Vertex(4, 1, 1);
+      const F = new Vertex(5, 2, 1);
+      const polygon = Polygon.fromPath([A, B, E, D]);
+      const square = new Square(B, C, E, F, null);
+      expect(Polygon.contains(polygon, square)).toEqual(false);
     });
   });
 });
--- src/app/models/polygon.ts
+++ src/app/models/polygon.ts
@@ -1,5 +1,6 @@
 import {Edge} from '@src/app/models/edge';
 import {Vertex} from '@src/app/models/vertex';
+import {Square} from '@src/app/models/square';
 
 export class Polygon {
   constructor(public edges: Edge[]) {
@@ -18,4 +19,29 @@ export class Polygon {
     return new Polygon(edges);
   }
 
+  /*
+    Checks whether the square is contained within the polygon. We use
+    a simplified version of the ray casting algorithm. To solve our
+    square-in-polygon problem, we cast a ray from the center of the square
+    to the right and count the number of intersections with the polygon.
+    If the number of intersections is odd, the square lies within the polygon.
+
+    See the test cases for examples.
+   */
+  public static contains(polygon: Polygon, square: Square): boolean {
+    const isVertical = (e: Edge) =>
+        e.source.x === e.target.x;
+    const squareIsLeftFromEdge = (s: Square, e: Edge) =>
+        s.topRight.x <= e.source.x;
+    const hasSameY = (s: Square, e: Edge) =>
+        Math.min(e.source.y, e.target.y) === square.topRight.y &&
+        Math.max(e.source.y, e.target.y) === square.bottomRight.y;
+
+    const numIntersections = polygon.edges
+        .filter(edge => isVertical(edge) && squareIsLeftFromEdge(square, edge) && hasSameY(square, edge))
+        .length;
+    return numIntersections % 2 === 1;
+  }
+
 }
+
