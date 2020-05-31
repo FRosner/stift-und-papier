43519cf27f5584b68921f6233a4819e4dbb7ea71
Frank Rosner <frank@fam-rosner.de>
2020-05-31 21:28:35 +0200

    format

--- src/app/models/edge.ts
+++ src/app/models/edge.ts
@@ -2,7 +2,11 @@ import {Vertex} from '@src/app/models/vertex';
 import {Player} from '@src/app/models/player';
 
 export class Edge {
-  constructor(public source: Vertex, public target: Vertex, public owner: Player | null) {
+  constructor(
+      public source: Vertex,
+      public target: Vertex,
+      public owner: Player | null,
+  ) {
   }
 
   public static initialize(source: Vertex, target: Vertex): Edge {
