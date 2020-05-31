483ef8409bca5227dd8263c7a90781ff00c7ab7b
Frank Rosner <frank@fam-rosner.de>
2020-05-31 16:00:36 +0200

    moar tests

--- src/app/components/game/game.component.spec.ts
+++ src/app/components/game/game.component.spec.ts
@@ -34,21 +34,45 @@ describe('GameComponent', () => {
 
   it('should render player edges with the respective color', () => {
     const player = new Player('test', 'blue');
-    component.drawEdge(
-        component.graph.edges.find(e => e.source.id === 0 && e.target.id === 1),
-        player,
-    );
+    component.graph.edges[0].owner = player;
     fixture.detectChanges();
 
     const edge = fixture.debugElement.query(By.css('#edge-0-0-1-1'));
     expect(edge.attributes['stroke']).toEqual(player.color);
   });
 
+  it('should render ownerless squares with the respective color', () => {
+    const edge = fixture.debugElement.query(By.css('#square-0-0'));
+    expect(edge.attributes['fill']).toEqual('#ccc');
+  });
+
+  it('should render player squares with the respective color', () => {
+    const player = new Player('test', 'blue');
+    const edge = fixture.debugElement.query(By.css('#square-0-0'));
+    component.squares[0].owner = player;
+    fixture.detectChanges();
+    expect(edge.attributes['fill']).toEqual(player.color);
+  });
+
   it('should change the owner of all squares after closing a section', () => {
-    // TODO
+    const player = new Player('test', 'blue');
+    drawEdge(player, 0, 0, 1, 0);
+    drawEdge(player, 1, 0, 1, 1);
+    drawEdge(player, 0, 1, 1, 1);
+    drawEdge(player, 0, 0, 0, 1);
+    expect(component.squares[0].owner).toEqual(player);
   });
 
   it('should not change the owner squares that already have an owner', () => {
     // TODO but needs multiplayer capabilities
   });
+
+  function drawEdge(player: Player, x1: number, y1: number, x2: number, y2: number) {
+    component.drawEdge(
+        component.graph.edges.find(e =>
+            e.source.x === x1 && e.source.y === y1 && e.target.x === x2 && e.target.y === y2,
+        ),
+        player,
+    );
+  }
 });
